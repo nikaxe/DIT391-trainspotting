c0531a3876b15aeef1177dc351c26821d06fe2fc
niklas axelsson <niklas.axelsson94@gmail.com>
2021-09-15 13:48:30 +0200

    Removed old code and added javadoc

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,25 +1,17 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Semaphore;
 
 import TSim.*;
 
-
 public class Lab1 {
   // Semaphores
-  Semaphore brown = new Semaphore(0, true);
-  Semaphore blue = new Semaphore(1, true);
-  Semaphore orange = new Semaphore(1, true);
-  Semaphore purple = new Semaphore(1, true);
-  Semaphore yellow = new Semaphore(1, true);
-  Semaphore red = new Semaphore(1, true);
-  Semaphore pink = new Semaphore(0, true);
-
-  //Map <Position, Sensor> sensors = new HashMap<>();
+  private final Semaphore brown = new Semaphore(0, true);
+  private final Semaphore blue = new Semaphore(1, true);
+  private final Semaphore purple = new Semaphore(1, true);
+  private final Semaphore yellow = new Semaphore(1, true);
+  private final Semaphore red = new Semaphore(1, true);
+  private final Semaphore pink = new Semaphore(0, true);
 
   public Lab1(int speed1, int speed2) {
-    
-    //putSensors();
     Thread train1 = new Train(false, 1, speed1, true);
     Thread train2 = new Train(true, 2, speed2, true);
 
@@ -27,12 +19,15 @@ public class Lab1 {
     train2.start();
   }
   
+  /**
+   * Train model class
+   */
   private class Train extends Thread {
-    private int id;
+    private final int id;
     private int maxSpeed;
     private boolean direction;
     private boolean onDefaultTrack;
-    TSimInterface tsi;
+    private final TSimInterface tsi;
 
     public Train(boolean dir, int id, int maxSpeed, boolean onDefaultTrack) {
       direction = dir;
@@ -42,6 +37,7 @@ public class Lab1 {
       tsi = TSimInterface.getInstance();
     }
 
+    // run() is run when start() is called on the Train instance.
     @Override
     public void run() {
       try {
@@ -50,16 +46,12 @@ public class Lab1 {
           SensorEvent se = tsi.getSensor(id);
           int x = se.getXpos();
           int y = se.getYpos();
-          /*
-          Sensor hitSensor = sensors.get(new Position(x, y));
-          if(se.getStatus() == SensorEvent.ACTIVE)
-            hitSensor.doAction(this, direction);
-          */
           
           if(se.getStatus() == SensorEvent.INACTIVE)
             continue; 
           Position sensorPos = new Position(x, y);
           
+          // Sensor cases
           // 1 or 2 
           if(sensorPos.equals(new Position(13, 3)) || sensorPos.equals(new Position(13, 5))) {
             if(direction)
@@ -132,13 +124,25 @@ public class Lab1 {
               changeDirection();
           }
         } 
+      // Catch CommandException or InterruptedException if any 
+      // methods throws
       } catch (Exception e) {
         e.printStackTrace();
         System.exit(1);
       }
     }
+
+    /**
+     * Used by the train when leaving a parallel track.
+     * @param toRelease the semaphore to release
+     * @param x X position of switch
+     * @param y Y position of switch
+     * @param releaseRight 
+     * @throws CommandException
+     */
     private void releaseIfDefaultTrack(Semaphore toRelease, int x, int y, boolean releaseRight) throws CommandException {
       if(onDefaultTrack){
+        // if on default track switch goes in given direction
         if(releaseRight)
           tsi.setSwitch(x, y, TSimInterface.SWITCH_RIGHT);
         else
@@ -146,15 +150,28 @@ public class Lab1 {
         toRelease.release();
         return;
       }
+      // if not on default track switch goes in the opposite direction 
       if(releaseRight)
         tsi.setSwitch(x, y, TSimInterface.SWITCH_LEFT);
       else
         tsi.setSwitch(x, y, TSimInterface.SWITCH_RIGHT);
     }
+
+    /**
+     * Used when a train needs to decide if it can use the default
+     * track on parallel sections.
+     * @param sem The semaphore to try to acquire.
+     * @param switchX X position of the switch before the semaphore.
+     * @param switchY Y position of the switch before the semaphore.
+     * @param switchRightOnAcquire True will set the switch to be in the right position 
+     * if the acquire succeds and in the left position if it fails. False does the reversed.
+     * @throws CommandException
+     */
     private void tryToAcquire(Semaphore sem, int switchX, int switchY
     , boolean switchRightOnAcquire)throws CommandException {
       if(sem.tryAcquire(1)){
         onDefaultTrack = true;
+        // if acquire succeds switch goes in the given direction
         if(switchRightOnAcquire)
           tsi.setSwitch(switchX, switchY, TSimInterface.SWITCH_RIGHT);
         else
@@ -162,12 +179,20 @@ public class Lab1 {
       }
       else{
         onDefaultTrack = false;
+        // if acquire failed switch goes in the other direction
         if(switchRightOnAcquire)
           tsi.setSwitch(switchX, switchY, TSimInterface.SWITCH_LEFT);
         else
           tsi.setSwitch(switchX, switchY, TSimInterface.SWITCH_RIGHT);
       }
     }
+
+    /**
+     * Stops the train and wait 3 seconds, 
+     * then start the train but in the opposite direction.
+     * @throws CommandException
+     * @throws InterruptedException
+     */
     private void changeDirection() throws CommandException, InterruptedException {
       tsi.setSpeed(id, 0);
       Thread.sleep(3000);
@@ -175,57 +200,42 @@ public class Lab1 {
       maxSpeed = -maxSpeed;
       tsi.setSpeed(id, maxSpeed);
     }
+
+    /**
+     * Set train speed to 0.
+     * @throws CommandException
+     */
     private void stopTrain() throws CommandException {
       tsi.setSpeed(id, 0);
     }
+
+    /**
+     * Set train speed to maximum.
+     * @throws CommandException
+     */
     private void startTrain() throws CommandException {
       tsi.setSpeed(id, maxSpeed);
     }
   }
-  /*
-  private interface SensorAction {
-    public void action(Train train) throws CommandException, InterruptedException;
-  }
 
-  private class Sensor {
-    private SensorAction upAction;
-    private SensorAction downAction;
-    public Sensor(SensorAction up, SensorAction down) {
-      upAction = up;
-      downAction = down;
-    }
-    public void doAction(Train train, boolean direction) throws CommandException, InterruptedException{
-      if(direction)
-        doUp(train);
-      else 
-        doDown(train);
-    }
-    private void doUp(Train train) throws CommandException, InterruptedException{
-      if(upAction != null)
-        upAction.action(train);
-    }
-    private void doDown(Train train) throws CommandException, InterruptedException{
-      if(downAction != null)
-        downAction.action(train);
-    }
-  }
-  */
+  /**
+   * Used to store and compare 2D positions  
+   */
   private class Position {
     private final int x;
     private final int y;
+
     public Position(int x, int y) {
       this.x = x;
       this.y = y;
     }
-    public int getX() { return x; }
-    public int getY() { return y; }
     
     @Override
     public boolean equals(Object obj) {
       if(obj == null || getClass() != obj.getClass())
         return false;
       Position other = (Position) obj;
-      return x == other.getX() && y == other.getY();
+      return x == other.x && y == other.y;
     }
     @Override
     public int hashCode() {
@@ -235,176 +245,3 @@ public class Lab1 {
     }
   }
 }
-
-// Ask ta if sensor map counts as shared variable 
-// good/bad idea to use bool default track in train
-
-/*
-CommandException - Exception in TSim
-Thrown when a command to TSim fails.
-CommandException(String) - Constructor for exception TSim.CommandException
-Constructs an CommandException with the specified detailed error message.
-*/
-
-
-/*
-direction == true is the upwards direction
-1:
-  direction: 
-    stop train
-    wait 2s
-    start train in the other direction
-  !direction: 
-    do nothing
-2:
-  direction: 
-    stop train
-    wait 2s
-    start train in the other direction
-  !direction:
-    do nothing
-3: 
-  direction:
-    release blue
-  !direction:
-    stop train
-    acquire blue
-    start train
-4: 
-  direction:
-    release blue
-  !direction:
-    stop train
-    acquire blue
-    acquire orange
-    start train
-5:
-  direction:
-    stop train
-    acquire blue
-    release orange
-    start train
-  !direction:
-    release blue
-6: 
-  direction: 
-    stop train
-    acquire blue
-    start train
-  !direction:
-    release blue
-7:
-  direction:
-    release purple
-  !direction:
-    stop train
-    acquire purple
-    set switch 1 right
-    release orange
-    start train
-8:
-  direction:
-    release purple
-  !direction:
-    stop train
-    acquire purple
-    set switch 1 left
-    start train
-9:
-  direction:
-    stop train
-    if(tryacquire orange)
-      set switch 1 right
-    else
-      set switch 1 left
-    start train
-  !direction:
-    do nothing
-10:
-  direction:
-    release yellow
-  !direction:
-    stop train
-    if(tryacquire yellow)
-      set switch 2 right
-    else
-      set switch 2 left
-    start train
-11:
-  direction:
-    stop train
-    acquire purple
-    set switch 2 right
-    start train
-  !direction:
-    release purple
-12:
-  direction:
-    stop train
-    acquire purple
-    set switch 2 left
-    start train
-  !direction:
-    release purple
-13:
-  direction:
-    release red
-  !direction:
-    stop train
-    acquire red
-    set switch 3 left
-    if(tryacquire pink)
-      set switch 4 left
-    else
-      set switch 4 right
-    start train
-14:
-  direction:
-    release red
-  !direction:
-    stop train
-    acquire red
-    set switch 3 right
-    if(tryacquire pink)
-      set switch 4 left
-    else
-      set switch 4 right
-    start train
-15:
-  direction:
-    stop train
-    acquire red
-    set switch 4 left
-    if(tryacquire yellow)
-      set switch 3 left
-    else
-      set switch 3 right
-    start train
-  !direction:
-    release red
-16:
-  direction:
-    stop train
-    acquire red 
-    set switch 4 right
-    if(tryacquire yellow)
-      set switch 3 left
-    else
-      set switch 3 right
-  !direction:
-    release red
-17:
-  direction:
-    do nothing
-  !direction:
-    stop train 
-    wait 3s
-    start train in other direction
-18:
-  direction:
-    do nothing
-  !direction:
-    stop train 
-    wait 3s
-    start train in other direction
-*/
